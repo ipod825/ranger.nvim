75ddecee92ea3bde30a39a9b39902d320b4247e7
Shih-Ming Wang <swang150@ucsc.edu>
2022-09-20 02:10:10 -0700

    Better trash.

--- lua/ranger/action/trash.lua
+++ lua/ranger/action/trash.lua
@@ -2,51 +2,80 @@ local M = {}
 local utils = require("ranger.action.utils")
 local transfer = require("ranger.action.transfer")
 local pathfn = require("libp.utils.pathfn")
-local Set = require("libp.datatype.Set")
-local Job = require("libp.Job")
+local Stack = require("libp.datatype.Stack")
+local vimfn = require("libp.utils.vimfn")
+local a = require("plenary.async")
+local fs = require("libp.fs")
+local uv = require("libp.fs.uv")
+local List = require("libp.datatype.List")
 
-local gio_available = vim.fn.executable("gio")
-if gio_available then
-	Job({ cmd = "gio trash --empty" }):start()
+local trash_dir = pathfn.join(vim.fn.stdpath("cache"), "ranger_trash")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = vim.api.nvim_create_augroup("ranger_trash_vimenter", {}),
+	callback = function()
+		a.void(function()
+			local _, err = fs.rmdir(trash_dir)
+			if err then
+				vimfn.error(err)
+			else
+				fs.mkdir(trash_dir)
+			end
+		end)()
+	end,
+})
+
+M._history = Stack()
+
+function M._trash_single(file_path)
+	local trash_path
+	local basename = pathfn.basename(file_path)
+	repeat
+		trash_path = ("%s/%s%s"):format(trash_dir, basename, pathfn.randomAlphaNumerical(10))
+	until not fs.is_readable(trash_path)
+
+	local _, err = uv.fs_rename(file_path, trash_path)
+	if err then
+		vimfn.error(err)
+		return
+	end
+
+	M._history:push({ file_path, trash_path })
 end
 
-local trash_cmd = function(abspath)
-	return gio_available and ('gio trash "%s"'):format(abspath) or ('rm "%s"'):format(abspath)
+function M._add_history_separation()
+	M._history:push({})
 end
-
-M._history = {}
 
 function M.trash_current()
 	local buffer, node = utils.get_cur_buffer_and_node()
-	local code = Job({ cmd = trash_cmd(node.abspath) }):start()
-	if code == 0 then
-		table.insert(M._history, { node.abspath })
+	if node.type == "header" then
+		return
 	end
+
+	M._add_history_separation()
+	M._trash_single(node.abspath)
 	buffer:rebuild_nodes()
 	buffer:draw()
 end
 
 function M.trash_selected()
-	local cmds = {}
-	local trashed_paths = {}
+	local trashed_paths = List()
 	for buffer, controller, node in transfer.selected_nodes() do
 		if node then
-			table.insert(cmds, trash_cmd(node.abspath))
-			table.insert(trashed_paths, node.abspath)
+			trashed_paths:append(node.abspath)
 			transfer.unselect_node(node, controller)
 		else
 			buffer:disable_fs_event_watcher()
-			local codes = Job.start_all(cmds)
-			local new_history = {}
-			for i, code in ipairs(codes) do
-				if code[1] == 0 then
-					table.insert(new_history, trashed_paths[i])
-				end
-			end
-			if #new_history > 0 then
-				table.insert(M._history, new_history)
-			end
-			trashed_paths = {}
+
+			M._add_history_separation()
+			a.util.join(trashed_paths:map(function(e)
+				return a.wrap(function(cb)
+					M._trash_single(e)
+					cb()
+				end, 1)
+			end))
+
 			buffer:enable_fs_event_watcher()
 			buffer:rebuild_nodes()
 			buffer:draw()
@@ -55,46 +84,29 @@ function M.trash_selected()
 end
 
 function M.restore_last()
-	if not gio_available then
-		vim.notify("Command gio not found!", vim.log.levels.WARN)
-		return
-	end
-	local restore_paths = table.remove(M._history, #M._history)
-	if not restore_paths then
-		return
-	end
-	restore_paths = Set(restore_paths)
+	local path_tuples = List()
+	local path_tuple
 
-	local lines = Job({ cmd = { "gio", "trash", "--list" } }):stdoutput()
-	local file_version = {}
-	for abspath in Set.values(restore_paths) do
-		file_version[abspath] = 1
-	end
-
-	for _, line in ipairs(lines) do
-		local gio_file, ori_path = unpack(vim.split(line, "\t"))
-		if Set.has(restore_paths, ori_path) then
-			local version = tonumber(gio_file:find_pattern("(%d*)$")) or 1
-			file_version[ori_path] = math.max(file_version[ori_path], version)
+	while true do
+		path_tuple = M._history:pop()
+		if not path_tuple or #path_tuple == 0 then
+			break
 		end
+		path_tuples:append(path_tuple)
 	end
 
-	local cmds = {}
-	for abspath in Set.values(restore_paths) do
-		if file_version[abspath] == 1 then
-			table.insert(cmds, { "gio", "trash", "--restore", "trash:///" .. pathfn.basename(abspath) })
-		else
-			table.insert(cmds, {
-				"gio",
-				"trash",
-				"--restore",
-				("trash:///%s.%d"):format(pathfn.basename(abspath), file_version[abspath]),
-			})
-		end
-	end
 	local buffer = utils.get_cur_buffer_and_node()
-	buffer:enable_fs_event_watcher()
-	Job.start_all(cmds)
+	buffer:disable_fs_event_watcher()
+
+	a.util.join(path_tuples:map(function(e)
+		return a.wrap(function(cb)
+			local ori_path, trash_path = unpack(e)
+			-- if not fs.is_directory(pathfn.dirname(ori_path))
+			uv.fs_rename(trash_path, ori_path)
+			cb()
+		end, 1)
+	end))
+
 	buffer:enable_fs_event_watcher()
 	buffer:rebuild_nodes()
 	buffer:draw()
